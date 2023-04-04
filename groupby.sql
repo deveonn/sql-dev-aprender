72f60da3600b19bc3ce45cad2bd538aa9757dfac
Eon222 <programmer.eon@gmail.com>
2023-04-04 12:00:36 -0300

    aula 13 finished - praticar mais desse conteudo(group by) etc

--- groupby.sql
+++ groupby.sql
@@ -15,7 +15,7 @@ SELECT col1, MAX(col2) FROM table1 GROUP BY col1
     t0dos os tens de metal e agrupa-los com group by. 
 */
 
-SELECT Produto,COUNT(Tipo) AS "QtdeDeMetal" 
+SELECT Produto, COUNT(Tipo) AS "QtdeDeMetal" 
 FROM TabelaDeProdutos 
 GROUP BY Produto
 
@@ -24,3 +24,46 @@ GROUP BY Produto
 SELECT FirstName, COUNT(FirstName) AS "QtdeDeNome" 
 FROM Person.Person 
 GROUP BY FirstName
+
+
+-- Media de um produto pela cor, (ex: produto azul custa em media X)
+SELECT color, AVG(ListPrice) AS "MEDIA"
+FROM Production.Product
+WHERE Color = 'Blue'
+GROUP BY Color;
+
+/*
+    DESAFIOS:
+    1 - Quantas pessoas tem o mesmo middle name, agrupadas pelo middle name?
+    2 - Qual é a quantidade que cada produto é vendido na loja?
+    3 - Quais foram as 10 vendas que no total tiveram os maiores valores de venda (line total)
+    por produto do maior valor para o menor
+    4 - Quantos produtos e qual a qtde media de produtos temos cadastrados nas nossas ordens de serviço
+    (WorkOrder), agrupados por productId
+
+*/
+
+-- 1
+SELECT MiddleName, COUNT(MiddleName) "QtdeNomeDoMeio"
+FROM Person.Person
+GROUP BY MiddleName;
+
+-- 2 
+
+SELECT TOP 10 SalesOrderDetailID, LineTotal
+FROM Sales.SalesOrderDetail
+ORDER BY LineTotal DESC
+
+
+-- 3 
+
+SELECT TOP 10 ProductId, SUM(LINETOTAL)
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+ORDER BY SUM(LineTotal) DESC
+
+-- 4 
+SELECT ProductId, COUNT(productId) AS "Contagem",
+AVG(orderqty) AS "MEDIA"
+FROM Production.WorkOrder
+GROUP BY ProductId
